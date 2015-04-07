49f42834401e18a1f51696c70be928da69990851
Pierre Jacomet <pjacomet@impinj.com>
2015-04-07 20:16:18 +0000

    Getting closer. JSON tests now succeed. Had to hack in jsonfixtures which was in 0.6.2. Still missing the UserResourceTests.

--- src/test/java/com/example/core/UserTests.java
+++ src/test/java/com/example/core/UserTests.java
@@ -1,6 +1,8 @@
 package com.example.core;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -8,13 +10,11 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-import java.util.Iterator;
+import java.io.IOException;
 import java.util.Set;
 
-//import static com.yammer.dropwizard.testing.JsonHelpers.asJson;
-//import static com.yammer.dropwizard.testing.JsonHelpers.fromJson;
-//import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
-
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class UserTests {
@@ -23,6 +23,18 @@ public class UserTests {
     private static final String EMPTY_ERROR_MESSAGE = "may not be empty";
 
     private static Validator validator;
+    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+    /**
+     * Loads the given fixture resource as a normalized JSON string.
+     *
+     * @param filename    the filename of the fixture
+     * @return the contents of {@code filename} as a normalized JSON string
+     * @throws IOException if there is an error parsing {@code filename}
+     */
+    private static String jsonFixture(String filename) throws IOException {
+        return MAPPER.writeValueAsString(MAPPER.readValue(fixture(filename), JsonNode.class));
+    }
 
     @BeforeClass
     public static void setUp() {
@@ -30,15 +42,17 @@ public class UserTests {
         validator = factory.getValidator();
     }
 
-//    @Test
-//    public void serializesToJson() throws Exception {
-//        assertEquals(jsonFixture("fixtures/user.json"), asJson(getUser()));
-//    }
-//
-//    @Test
-//    public void deserializesFromJSON() throws Exception {
-//        assertEquals(getUser(), fromJson(jsonFixture("fixtures/user.json"), User.class));
-//    }
+    @Test
+    public void serializesToJson() throws Exception {
+        final User user = getUser();
+        assertThat(MAPPER.writeValueAsString(user)).isEqualTo(jsonFixture("fixtures/user.json"));
+    }
+
+    @Test
+    public void deserializesFromJSON() throws Exception {
+        final User user = getUser();
+        assertThat(MAPPER.readValue(fixture("fixtures/user.json"), User.class)).isEqualToComparingFieldByField(user);
+    }
 
     // Should be replaced with individual class field validator tests
     @Test
